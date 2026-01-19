39b699a71dc9082c5be9fb8a9a401b985c6da904
prog4th <prog4th@proton.me>
2026-01-20 03:21:56 +0900

    🗃️MongoDB 연결

---
+++ src/main/kotlin/kr/ac/kumoh/s20260000/spring12jwt/Spring12JwtApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20260000.spring12jwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring12JwtApplication
+
+fun main(args: Array<String>) {
+    runApplication<Spring12JwtApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20260000/spring12jwt/Spring12JwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20260000.spring12jwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Spring12JwtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
